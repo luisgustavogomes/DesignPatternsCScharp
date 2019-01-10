d2922081b535b88346eb876652044b45e6e3baad
luis gomes <luisgustavogomes@outlook.com>
2019-01-10 07:57:50 -0200

    Strategy

---
+++ CursoDesignPatterns/CalculadorDeImpostos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesignPatterns
+{
+    public class CalculadorDeImpostos
+    {
+        public static void RealizaCalculo(Orcamento orcamento, IImposto imposto)
+        {
+            double valor = imposto.Calcula(orcamento);
+            Console.WriteLine(valor);
+        }
+    }
+}
--- CursoDesignPatterns/Program.cs
+++ CursoDesignPatterns/Program.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace CursoDesignPatterns
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            IImposto iss = new ISS();
+            IImposto icms = new ICMS();
+
+            Orcamento orcamento = new Orcamento(120000.00);
+
+            CalculadorDeImpostos.RealizaCalculo(orcamento, iss);
+            CalculadorDeImpostos.RealizaCalculo(orcamento, icms);
+
+
+            Console.ReadLine();
         }
     }
 }
